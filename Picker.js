b001843ea871c04da620da94d042254c7d90df3c
Sing Yeung <ksyeung@ebsl.hk>
2017-08-28 18:07:45 +0800

    code bugfix

--- Picker.js
+++ Picker.js
@@ -36,7 +36,7 @@ export default class Picker extends Component {
       }
       return(
         <TouchableOpacity
-          onPress={::this.handlePress}
+          onPress={this.handlePress.bind(this)}
           style={{
             alignSelf: 'stretch',
             alignItems: 'center',
